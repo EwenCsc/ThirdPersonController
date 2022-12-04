46a54018fd472824ba1ab40000b1f85840668919
EwenCsc <cossec.ewen@gmail.com>
2022-12-04 12:49:59 +0100

    First commit as a package

---
+++ Runtime/Controller/Camera/ThirdPersonCameraController.cs
@@ -0,0 +1,49 @@
+namespace Ewengine.ThirdPersonController
+{
+	using UnityEngine;
+	using UnityEngine.InputSystem;
+
+	public class ThirdPersonCameraController : MonoBehaviour
+	{
+		#region Fields
+		#region Serialized Fields
+		[SerializeField] private ThirdPersonCharacterController _characterController = null;
+		[SerializeField] private Transform _followedTransform = null;
+
+		[SerializeField][Range(1.0f, 10.0f)] private float _sensibility = 5.0f;
+		[SerializeField][Range(1.0f, 10.0f)] private float _distanceToOrigin = 5.0f;
+
+		[SerializeField] Vector2 _pitchMinMax = new Vector2(-70.0f, 70.0f);
+		[SerializeField] AnimationCurve _pitchAcceleration = new AnimationCurve();
+
+		[SerializeField][Range(0.0f, 1.0f)] private float _rotationSmoothTime = 0.1f;
+		#endregion Serialized Fields
+
+		#region Internal Fields
+		private float _yaw = 0.0f;
+		private float _pitch = 0.0f;
+		private Vector3 _currentRotation = Vector3.zero;
+		private Vector3 _rotationSmoothDamp = Vector3.zero;
+		#endregion Internal Fields
+		#endregion Fields
+
+		#region Methods
+		private void Update()
+		{
+			InputAction lookAction = _characterController.LookAction;
+			Vector2 lookInput = lookAction.ReadValue<Vector2>();
+
+			_yaw += lookInput.x * _sensibility;
+			_pitch -= lookInput.y * _sensibility * _pitchAcceleration.Evaluate(Mathf.InverseLerp(_pitchMinMax.x, _pitchMinMax.y, _pitch));
+			_pitch = Mathf.Clamp(_pitch, _pitchMinMax.x, _pitchMinMax.y);
+
+			transform.position = _followedTransform.position;
+
+			_currentRotation = Vector3.SmoothDamp(_currentRotation, new Vector3(_pitch, _yaw), ref _rotationSmoothDamp, _rotationSmoothTime);
+			transform.eulerAngles = _currentRotation;
+
+			transform.position += -transform.forward * _distanceToOrigin;
+		}
+		#endregion Methods
+	}
+}
---
+++ Runtime/Controller/Character/ThirdPersonCharacterAnimator.cs
@@ -0,0 +1,36 @@
+﻿namespace Ewengine.ThirdPersonController
+{
+	using UnityEngine;
+
+	public class ThirdPersonCharacterAnimator : MonoBehaviour
+	{
+		#region Fields
+		[SerializeField] private ThirdPersonCharacterController _characterController = null;
+
+		[SerializeField] private Animator _animator = null;
+
+		[SerializeField] private string _forwardVelocityParameter = string.Empty;
+		[SerializeField] private string _rightdVelocityParameter = string.Empty;
+		[SerializeField] private string _isJumpingParameter = string.Empty;
+		[SerializeField] private string _isGroundedParameter = string.Empty;
+		#endregion Fields
+
+		#region Methods
+		private void LateUpdate()
+		{
+			Transform characterTransform = _characterController.transform;
+			Vector3 velocity = _characterController.Rigidbody.velocity.normalized;
+
+			_animator.SetFloat(_forwardVelocityParameter, Vector3.Dot(characterTransform.forward, velocity));
+			_animator.SetFloat(_rightdVelocityParameter, Vector3.Dot(characterTransform.right, velocity));
+
+			if (_characterController.IsJumping)
+			{
+				_animator.SetTrigger(_isJumpingParameter);
+			}
+
+			_animator.SetBool(_isGroundedParameter, _characterController.IsGrounded());
+		}
+		#endregion Methods
+	}
+}
